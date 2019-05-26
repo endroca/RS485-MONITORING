f7f98e8e98d5be4276bf8db00c9cc4d82c0a90bf
Andrew <eu@andrewneto.com>
2019-05-26 17:54:41 -0300

    microservico tcp implementado (alpha)

--- server/service/node/tcp.js
+++ server/service/node/tcp.js
@@ -1,17 +1,92 @@
+require('dotenv').config({ path: '../../.env' });
 const net = require('net');
-const network = require('network');
+const defaultGateway = require('default-gateway');
 const readline = require('readline');
+const mongoose = require('mongoose');
+const socket = require('socket.io-client')('http://localhost:3000');
 
-const client = new net.Socket();
-client.connect(80, '192.168.4.1');
+const IPGateway = defaultGateway.v4.sync();
 
-const line = readline.createInterface(client, client);
+/*
+* DB Connection
+*/
+let userAndPass = '';
+if (process.env.MONGO_USER != '') {
+    userAndPass = process.env.MONGO_USER + ':' + process.env.MONGO_USER_PASS + '@';
+}
 
-line.on('line', (line) => {
-    console.log(line);
+mongoose.connect('mongodb://' + userAndPass + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_SCHEMA, { useCreateIndex: true, useNewUrlParser: true }, (err, response) => {
+    if (err) {
+        console.log("Erro in connect db: ", err);
+    } else {
+        console.log("Connection has been added: mongoDB server");
+    }
 });
 
 
-client.on('close', () => {
-    console.log("exit");
-})
+/*
+* DB Models
+*/
+require('../../models/Sensors');
+
+
+/*
+* Functions
+*/
+const functions = require('./functions');
+
+/*
+* Connection and Parser
+*/
+const client = new net.Socket();
+const line = readline.createInterface(client, client);
+
+
+client.connect(80, IPGateway.gateway, () => {
+    client.write(functions.requestFunction(2));
+});
+
+
+line.on('line', ($line) => {
+    try{
+        line = JSON.parse($line);
+    }catch(e){
+        console.log($line);
+    }
+    
+    console.log(line);
+
+    if ("action" in line) {
+        //model -> {"action":2,"response":[{"id":"S1","configs":[1000,-1]}]}
+        switch (line.action) {
+            case 2:
+                functions.responseFunctions(2, line.response, socket.emit('action', 2));
+                break;
+        }
+    } else {
+        socket.emit('read', line);
+        console.log('Sendo room(read): ', line);
+    }
+});
+
+socket.on('action', (msg) => {
+    switch(msg.action){
+        case 1:
+            msg.message["action"] = 1;
+            port.write(JSON.stringify(msg.message));
+            break;
+        case 2:
+            port.write(functions.requestFunction(2));
+            break;
+    }
+});
+
+socket.on('connect', () => {
+    /*if (!port.isOpen) {
+        port.open();
+    }*/
+});
+
+socket.on('disconnect', () => {
+    //port.close();
+});
